9401ddb973a90c3d9b6918b0bb68973d146f823e
allbutgold <contact@leokieffer.com>
2023-01-05 15:50:34 +0100

    styling

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+/* function darkMode(){
+    document.querySelector(".body").style.backgroundColor = "black";
+}
+ */
+document.getElementById("darkMode").addEventListener("click", ()=>{
+    if (document.body.style.backgroundColor === "white") {
+        (document.body.style.backgroundColor = "black")
+        
+    } else {
+        
+    }
+})
