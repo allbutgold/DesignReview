f47192d88ff4bbb59a6f632c31df74647e4808c9
allbutgold <contact@leokieffer.com>
2023-01-05 16:39:08 +0100

    still trying to fix this darkMode button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 
-
-/* function darkMode(){
-    document.querySelector(".body").style.backgroundColor = "black";
-}
- */
 document.getElementById("darkMode").addEventListener("click", ()=>{
     if (document.body.style.backgroundColor === "white") {
-        (document.body.style.backgroundColor = "black")
+        document.body.style.backgroundColor = "black"
+        document.body.style.color = "white"
+        
+        
         
     } else {
-        
+        document.body.style.backgroundColor = "white"
+        document.body.style.color = "black"
     }
 })
