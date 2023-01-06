dd2b485c5c2db0eb7d13f483e7fd502b14758bf5
allbutgold <contact@leokieffer.com>
2023-01-06 15:29:17 +0100

    starting media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
+const blackText = document.querySelectorAll("a");
+/* for (let i = 0; i < blackText.length; i++)  */
+
 
 document.getElementById("darkMode").addEventListener("click", ()=>{
     if (document.body.style.backgroundColor === "white") {
         document.body.style.backgroundColor = "black"
         document.body.style.color = "white"
-        
-        
+        blackText[0].style.color = "white"
         
     } else {
         document.body.style.backgroundColor = "white"
         document.body.style.color = "black"
+        blackText[0].style.color = "black"
     }
 })
